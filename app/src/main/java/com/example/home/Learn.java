743b4b9414820dad30998304b57722f7a6d4b58e
BoomBakar <abubakardar608@gmail.com>
2022-05-20 03:58:01 +0500

    Learning module Completed

--- app/src/main/java/com/example/home/Learn.java
+++ app/src/main/java/com/example/home/Learn.java
@@ -15,46 +15,266 @@ public class Learn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
 
-        buttons = new Button[26];
-        buttons[0] = findViewById(R.id.btnA);
-        buttons[1] = findViewById(R.id.btnB);
-        buttons[2] = findViewById(R.id.btnC);
-        buttons[3] = findViewById(R.id.btnD);
-        buttons[4] = findViewById(R.id.btnE);
-        buttons[5] = findViewById(R.id.btnF);
-        buttons[6] = findViewById(R.id.btnG);
-        buttons[7] = findViewById(R.id.btnH);
-        buttons[8] = findViewById(R.id.btnI);
-        buttons[9] = findViewById(R.id.btnJ);
-        buttons[10] = findViewById(R.id.btnK);
-        buttons[11] = findViewById(R.id.btnL);
-        buttons[12] = findViewById(R.id.btnM);
-        buttons[13] = findViewById(R.id.btnN);
-        buttons[14] = findViewById(R.id.btnO);
-        buttons[15] = findViewById(R.id.btnP);
-        buttons[16] = findViewById(R.id.btnQ);
-        buttons[17] = findViewById(R.id.btnR);
-        buttons[18] = findViewById(R.id.btnS);
-        buttons[19] = findViewById(R.id.btnT);
-        buttons[20] = findViewById(R.id.btnU);
-        buttons[21] = findViewById(R.id.btnV);
-        buttons[22] = findViewById(R.id.btnW);
-        buttons[23] = findViewById(R.id.btnX);
-        buttons[24] = findViewById(R.id.btnY);
-        buttons[25] = findViewById(R.id.btnZ);
-        for (; i < buttons.length; i++) {
-            buttons[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    String text = buttons.toString();
-                    Intent intent=new Intent(Learn.this,LearningImages.class);
-                    intent.putExtra("Alphabet",text);
-                    startActivity(intent);
-                }
-            });
-        }
 
-
-
+        Button btnA = findViewById(R.id.btnA);
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnA.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnB = findViewById(R.id.btnB);
+        btnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnB.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnC = findViewById(R.id.btnC);
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnC.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnD = findViewById(R.id.btnD);
+        btnD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnD.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnE = findViewById(R.id.btnE);
+        btnE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnE.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnF = findViewById(R.id.btnF);
+        btnF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnF.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnG = findViewById(R.id.btnG);
+        btnG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnG.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnH = findViewById(R.id.btnH);
+        btnH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnH.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnI = findViewById(R.id.btnI);
+        btnI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnI.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnJ = findViewById(R.id.btnJ);
+        btnJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnJ.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnK = findViewById(R.id.btnK);
+        btnK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnK.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnL= findViewById(R.id.btnL);
+        btnL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnL.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnM = findViewById(R.id.btnM);
+        btnM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnM.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnN = findViewById(R.id.btnN);
+        btnN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnN.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnO = findViewById(R.id.btnO);
+        btnO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnO.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnP = findViewById(R.id.btnP);
+        btnP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnP.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnQ = findViewById(R.id.btnQ);
+        btnQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnQ.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnR = findViewById(R.id.btnR);
+        btnR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnR.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnS = findViewById(R.id.btnS);
+        btnS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnS.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnT = findViewById(R.id.btnT);
+        btnT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnT.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnU = findViewById(R.id.btnU);
+        btnU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnU.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnV = findViewById(R.id.btnV);
+        btnV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnV.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnW = findViewById(R.id.btnW);
+        btnW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnW.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnX = findViewById(R.id.btnX);
+        btnX.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnX.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnY = findViewById(R.id.btnY);
+        btnY.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnY.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
+        Button btnZ = findViewById(R.id.btnZ);
+        btnZ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = btnZ.getText().toString();
+                Intent intent=new Intent(Learn.this,LearningImages.class);
+                intent.putExtra("Alphabet",text);
+                startActivity(intent);
+            }
+        });
     }
 }
