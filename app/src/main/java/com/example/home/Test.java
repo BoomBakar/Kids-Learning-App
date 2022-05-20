67d231443636f006a6ae7caeb3a6f651e2f31462
BoomBakar <abubakardar608@gmail.com>
2022-05-20 06:21:26 +0500

    Random Image Function added

--- app/src/main/java/com/example/home/Test.java
+++ app/src/main/java/com/example/home/Test.java
@@ -1,14 +1,31 @@
 package com.example.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class Test extends AppCompatActivity {
+
+    ImageView im;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        getImg();
     }
+    public void getImg()
+    {
+
+        Random random = new Random();
+        char randChar = (char) (random.nextInt(26) + 'a');
+        im=findViewById(R.id.img);
+        String ran = String.valueOf(randChar);
+        im.setImageResource(getResources().getIdentifier(ran,"drawable",getPackageName()));
+    }
+
 }
